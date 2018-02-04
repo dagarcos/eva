a6c4e5449fcd62796d437f2965018e4396f33c40
dagarcos <daniel.garcia@uv.es>
2018-02-04 19:18:51 +0100

    Add files via upload

---
+++ problemas.js
@@ -0,0 +1,3 @@
+
+
+var problemas = ['1.Magdalenas', '3.Cumpleanyos', '11.Pared', '12.Familia', '21.Supermercado', '22.Jardin', '24.Robots', '30.Dibujo'];
---
+++ problemas/1.Magdalenas/1.Magdalenas.js
@@ -0,0 +1,18 @@
+{
+	titulo : '\u00a1DECORAMOS MAGDALENAS!', 
+	encabezado : 'Nos han invitado a una fiesta de los sabores y cada invitado debe llevar una comida. Nosotros hemos decidido llevar magdalenas. Vamos sac\u00e1ndolas poco a poco del horno y coloc\u00e1ndolas de la siguiente forma:', 
+	apartados : {
+		a : 'a) \u00bfCu\u00e1ntas habr\u00e1 la cuarta vez que saquemos magdalenas del horno?', 
+		b : 'b) \u00bfCu\u00e1ntas habr\u00e1 la d\u00e9cima vez que saquemos magdalenas del horno? \u00bfC\u00f3mo lo sabes?', 
+		c : 'c) \u00bfCu\u00e1ntas tendremos cuando saquemos magdalenas del horno por 50 vez? \u00bfC\u00f3mo lo sabes?', 
+		d : 'd) \u00bfC\u00f3mo le explicar\u00edas a un amigo c\u00f3mo puede calcular el n\u00famero de magdalenas que salen en cada horneada?', 
+		e : 'e) Voy de nuevo a la cocina m\u00e1s tarde y veo que est\u00e1n sacando 104 magdalenas del horno. \u00bfCu\u00e1ntas veces se han cocinado magdalenas?'}, 
+	soluciones : {a : '8', b : '20', c : '100', d : '', e : '52'}, 
+	ruta_patron : 'problemas\/1.Magdalenas\/patron.png', 
+	ruta_unidades : {
+		1 : 'problemas\/1.Magdalenas\/unidad_1.png', 
+		2 : 'problemas\/1.Magdalenas\/unidad_2.png', 
+		3 : 'problemas\/1.Magdalenas\/unidad_3.png', 
+		4 : 'problemas\/1.Magdalenas\/unidad_4.png'}
+}
+
